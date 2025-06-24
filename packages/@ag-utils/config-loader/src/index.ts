88c522628d5d4c8f77ae2b200c62bd293f7e0d4a
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-24 18:54:17 +0900

    feat: add index.ts for main entry point
    - index.ts: add entry point and barrel files

---
+++ packages/@ag-utils/config-loader/src/index.ts
@@ -0,0 +1,9 @@
+// src: index.ts
+// @(#): main entry point / barrel file
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export * from './findConfigFile';
+export * from './parser/parseConfig';
