9bcb7fbbf24f0282a181e1d1fdc095c94a571ba5
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-24 22:10:09 +0900

    config: initialize and update configuration files
    - Add comprehensive default configs for gitignore, cspell, commitlint, ESLint, secretlint, tsup, and Vitest
    - Update package version and extend .gitignore to exclude temp directories
    - Adjust tsup and Vitest configs for temp ignoring and CI examples
    - Disable import/order rule in ESLint config as checked by dprint

---
+++ packages/@ag-utils/e2e-framework/configs/tsup.config.module.ts
@@ -0,0 +1,26 @@
+// src: configs/tsup.config.module.ts
+// @(#) : tsup config for esm module
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// system config
+import { defineConfig } from 'tsup';
+
+// user config
+import { baseConfig } from '../../../../shared/configs/tsup.config.base';
+
+// configs
+export default defineConfig({
+  ...baseConfig,
+
+  // sub-packages definition
+  format: ['esm'],
+  outDir: 'module', // for ESM
+  // tsconfig
+  tsconfig: './tsconfig.json',
+  // exclude temporary directories
+  exclude: ['temp/**/*'],
+});
