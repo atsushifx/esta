b0ad3beb32f8cf47f5eb181bab509186cb818beb
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-27 11:49:09 +0900

    feat(text-parser): .txt用にplaintext parserを追加
    - common.types.ts: .txt拡張子を追加
    - parseText.ts: パーサーで'txt'拡張子を処理するためにparsePlainTextを統合
    - parser/parsePlainText.ts: テキストをそのまま返すparsePlainText関数を実装
    - __tests__/parseText.spec.ts: 空および複数行のケースを含むプレーンテキスト'txt'拡張子解析のテストを追加
    - parser/__tests__/parsePlainText.spec.ts: テキストのパススルー動作を検証するparsePlainText関数の単体テストを追加

---
+++ packages/@ag-utils/text-parser/src/parser/parsePlainText.ts
@@ -0,0 +1,11 @@
+// src: ./parser/parsePlainText.ts
+// プレーンテキストパーサー
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export const parsePlainText = (text: string): string => {
+  return text;
+};
