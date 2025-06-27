8014415e548339e1f1670aab988bec18ddde8fa9
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-27 10:22:46 +0900

    feat(text-parser): initialize text-parser package's config files

---
+++ packages/@ag-utils/text-parser/configs/vitest.config.ci.ts
@@ -0,0 +1,40 @@
+// src: ./configs/vitest.config.ci.ts
+// @(#) : vitest config for integration test
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// libs for base directory
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+// base directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// system config
+import { mergeConfig } from 'vitest/config';
+
+// user common config
+import baseConfig from '../../../../shared/configs/vitest.config.base';
+
+// config
+export default mergeConfig(baseConfig, {
+  test: {
+    include: [
+      // CI Tests
+      'tests/**/*.test.ts',
+      'tests/**/*.spec.ts',
+    ],
+    caches: {
+      dir: path.resolve(__dirname, '../../../../.cache/vitest-cache/ci/'),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      '@shared': path.resolve(__dirname, '../shared'),
+    },
+  },
+});
