148d4e7bd1f8760d65b201823322d72381f41200
Furukawa Atsushi <atsushifx@gmail.com>
2025-07-06 01:07:55 +0900

    chore: ESLint設定プロジェクトからe2e-fixture-frameworkを削除
    - configs/eslint.projects.js: ESLintプロジェクトリストからe2e-fixture-frameworkを削除

--- configs/eslint.projects.js
+++ configs/eslint.projects.js
@@ -12,7 +12,6 @@ export default [
   './packages/@ag-utils/command-utils/tsconfig.json',
   './packages/@ag-utils/config-loader/tsconfig.json',
   './packages/@ag-utils/common/tsconfig.json',
-  './packages/@ag-utils/e2e-fixture-framework/tsconfig.json',
   './packages/@ag-utils/text-parser/tsconfig.json',
   './packages/@ag-system/ag-logger/tsconfig.json',
   './packages/@ag-actions/tool-installer/tsconfig.json',
