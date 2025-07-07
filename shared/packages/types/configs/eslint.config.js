f91b112494639e50c66a6d04391e39bc7519cf85
Furukawa Atsushi <atsushifx@gmail.com>
2025-07-07 23:23:32 +0900

    config: /shared/types: パッケージ用初期configを作成

---
+++ shared/packages/types/configs/eslint.config.js
@@ -0,0 +1,33 @@
+// src: shared/common/configs/eslint.config.js
+// @(#) : ESLint flat config for TypeScript workspace
+//
+// Copyright (c) 2025 atsushifx <https://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// libs
+
+// import form common base config
+import baseConfig from '../../../../base/configs/eslint.config.base.js';
+
+// settings
+export default [
+  ...baseConfig,
+
+  // source code settings
+  {
+    files: [
+      'index.ts',
+      'constants/**/*.ts',
+      'types/**/*.ts',
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+];
