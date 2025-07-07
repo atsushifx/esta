800c7d336041977845f40b2e78833bd32599a465
Furukawa Atsushi <atsushifx@gmail.com>
2025-07-08 03:09:30 +0900

    config: initial config commit for @esta-core/feature-flags
    - feature-flagsパッケージ向けに各種設定ファイルを初期設定

---
+++ packages/@esta-core/feature-flags/shared/types/index.ts
@@ -0,0 +1,9 @@
+// src: /shared/types/index.ts
+// @(#) : types definition barrel file
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export {};
---
+++ packages/@esta-core/feature-flags/src/index.ts
@@ -0,0 +1,9 @@
+// src: ./src/index.ts
+// @(#) : @esta-core: ESTA Feature Flags
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export {};
