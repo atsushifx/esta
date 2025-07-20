2447e48eb4f1b00bc028edde7149148d3aaf9dc4
Furukawa Atsushi <atsushifx@gmail.com>
2025-07-20 17:06:12 +0900

    feat(esta-config): build用にからのindex.tsを追加

---
+++ packages/@esta-core/esta-config/src/index.ts
@@ -0,0 +1,10 @@
+// src: index.ts
+// @(#) : barrel file
+//
+// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// import all
+export {};
