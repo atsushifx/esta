e8ac05957a04b7ee6db2c4ffcd3254d531610b11
Furukawa Atsushi <atsushifx@gmail.com>
2025-08-25 21:07:52 +0900

    docs(eslint): ESLintのtsconfigパスにわかりやすさのため日本語コメントを追加
    - ESLintプロジェクトのすべてのtsconfigパスに説明的な日本語コメントを追加

--- configs/eslint.projects.js
+++ configs/eslint.projects.js
@@ -8,25 +8,25 @@
 
 export default [
   // for OSS Utils
-  './packages/@agla-e2e/fileio-framework/tsconfig.json',
-  './packages/@agla-utils/ag-logger/tsconfig.json',
+  './packages/@agla-e2e/fileio-framework/tsconfig.json', // E2Eテスト用ファイルI/Oフレームワーク
+  './packages/@agla-utils/ag-logger/tsconfig.json', // 構造化ログフレームワーク
   // shared packages
-  './shared/packages/constants/tsconfig.json',
-  './shared/packages/types/tsconfig.json',
+  './shared/packages/constants/tsconfig.json', // esta 共通定数
+  './shared/packages/types/tsconfig.json', // esta 共通型
   // esta-error
-  'packages/@esta-error/error-handler/tsconfig.json',
-  'packages/@esta-error/error-result/tsconfig.json',
+  'packages/@esta-error/error-handler/tsconfig.json', // 集約エラーハンドリング・終了管理
+  'packages/@esta-error/error-result/tsconfig.json', // エラー結果型
   // esta-core
-  'packages/@esta-core/feature-flags/tsconfig.json',
-  'packages/@esta-core/tools-config/tsconfig.json',
+  'packages/@esta-core/feature-flags/tsconfig.json', // フィーチャーフラグ管理システム
+  'packages/@esta-core/tools-config/tsconfig.json', // ツールインストール設定生成器
   // esta-system
-  'packages/@esta-system/exit-status/tsconfig.json',
+  'packages/@esta-system/exit-status/tsconfig.json', // システム終了ステータス管理
   // utils
-  './packages/@esta-utils/command-runner/tsconfig.json',
-  './packages/@esta-utils/get-platform/tsconfig.json',
-  './packages/@esta-utils/config-loader/tsconfig.json',
+  './packages/@esta-utils/command-runner/tsconfig.json', // コマンド存在チェック・実行ユーティリティ
+  './packages/@esta-utils/get-platform/tsconfig.json', // プラットフォーム検出ユーティリティ
+  './packages/@esta-utils/config-loader/tsconfig.json', // 汎用設定ファイルローダー
   // gha modules
-  './packages/@esta-actions/tools-installer/tsconfig.json',
+  './packages/@esta-actions/tools-installer/tsconfig.json', // egetを使った汎用ツールインストーラーフレームワーク
   // main
-  './tsconfig.json',
+  './tsconfig.json', // ルートプロジェクト設定
 ];
